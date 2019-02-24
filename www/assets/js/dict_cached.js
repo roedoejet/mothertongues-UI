60567e50e474108b1f6660a5bcc6fb35e67eee1e
roedoejet <aidanpine@shaw.ca>
2019-02-24 10:01:47 -0800

    fixed onerror img loading

--- www/assets/js/dict_cached.js
+++ www/assets/js/dict_cached.js
@@ -1,4 +1,4 @@
 var dataDict = [
     { "source": "CustomizeYourSource1", "definition": "sample definition 1", "optional": [{ "Sample Optional Information": "Unicode works: ❤ ☀ ☆ ☂ ☻ ♞ ☯ ☭ ☢ €" }], "word": "sample word 1", "sorting_form": [18], "entryID": "1", "audio": [{"filename": "https://www2.cs.uic.edu/~i101/SoundFiles/StarWars60.wav"}] },
-    { "source": "CustomizeYourSource2", "definition": "sample definition 2", "optional": [{ "Sample Optional Information": "Unicode works: ❤ ☀ ☆ ☂ ☻ ♞ ☯ ☭ ☢ €" }], "word": "sample word 2", "sorting_form": [18], "entryID": "2" }
+    { "source": "CustomizeYourSource2", "definition": "sample definition 2", "optional": [{ "Sample Optional Information": "Unicode works: ❤ ☀ ☆ ☂ ☻ ♞ ☯ ☭ ☢ €" }], "word": "sample word 2", "sorting_form": [18], "entryID": "2", "img": "https://via.placeholder.com/225?text=Sample+MTD+Image" }
 ]
