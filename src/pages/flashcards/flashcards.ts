05bc7404588189a539c84f40501a1d4419bf3dc0
roedoejet <aidanpine@shaw.ca>
2019-11-09 10:58:29 -0800

    updated flashcard

--- src/pages/flashcards/flashcards.ts
+++ src/pages/flashcards/flashcards.ts
@@ -6,6 +6,8 @@ import { Flashcard } from './flashcard-modal.component'
 
 import { MTDService } from '../../app/mtd.service'
 
+import { MTDInfo } from '../../app/global'
+
 @Component({
   selector: 'page-flashcards',
   templateUrl: 'flashcards.html'
@@ -16,14 +18,16 @@ export class Flashcards {
   decks: string[];
   deckSelectOptions: Object = { title: "Select a Deck" };
   selectedFlashcardStyle: string;
+  language: string = MTDInfo.config['L1']['name']
+  language2: string = MTDInfo.config['L2']['name']
 
   constructor(public navCtrl: NavController, public modalCtrl: ModalController, public mtdService: MTDService, private alertCtrl: AlertController) {
     this.decks = Object.keys(mtdService.categories)
     this.flashcardStyles = [
-    { "title": "Passive", "info": "This is the easiest method. It involves seeing the {{name}} word and guessing English." },
-    { "title": "Active", "info": "This method is designed to test your spelling of the {{ name }} word. You are provided with the English, and have to guess the {{ name } } word." },
-    { "title": "Non-Written", "info": "This method is entirely without any written prompt. Try and guess the word in both English and {{ name }}!" }
-  ]
+      { "title": `Show ${this.language}`, "info": `This is the easiest method. It involves seeing the ${this.language} word and guessing ${this.language2}.` },
+      { "title": `Show ${this.language2}`, "info": `This method is designed to test your spelling of the ${this.language} word. You are provided with the ${this.language2}, and have to guess the ${this.language} word.` },
+      { "title": "Audio Only", "info": `This method is entirely without any written prompt. Try and guess the word in both ${this.language2} and ${this.language}!` }
+    ]
   }
 
   startFlashcards() {
