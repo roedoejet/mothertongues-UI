da0156b8f40dda763b90cfab82a87205ef0e0da0
roedoejet <aidanpine@shaw.ca>
2020-01-11 14:47:11 -0800

    fv build

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -10,7 +10,17 @@ import { MTDInfo } from '../../app/global'
 })
 export class About {
   language: string = MTDInfo.config['L1']['name']
+  build: string;
   constructor(public navCtrl: NavController) {
+    this.build = this.buildToString(MTDInfo.config['build'])
+  }
+
+  buildToString(build: string){
+    let year = build.slice(0,5)
+    let month = build.slice(5,7)
+    let day = build.slice(7,9)
+    let time = build.slice(9,11) + ":" + build.slice(11,13)
+    return `at ${time} on ${year}/${month}/${day}`
   }
 
 }
